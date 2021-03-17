1634de6c728468ef543ba861fd7bd1702b734d84
plam <plam@localhost.dev>
2021-03-17 19:21:26 +0000

    04_03 What useState Represents in a Class Component

---
+++ src/InputElementClass.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class InputElementClass extends React.Component {
+  state = {
+    inputText: "",
+  };
+  handleChange = (event) => {
+    this.setState({
+      inputText: event.target.value,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleChange} />
+        <br />
+        {this.state.inputText}
+      </div>
+    );
+  }
+}
+
+export default InputElementClass;
